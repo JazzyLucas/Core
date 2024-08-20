0e92c16fbfc476c43c999618db13f184481f7d93
JazzyLucas <59397873+JazzyLucas@users.noreply.github.com>
2024-08-20 00:09:16 -0400

    Update PerspectiveController.cs
    to raycast and adjust 3rd person distance

--- Assets/Plugins/JazzyLucas/Core/Controllers/PerspectiveController.cs
+++ Assets/Plugins/JazzyLucas/Core/Controllers/PerspectiveController.cs
@@ -1,24 +1,24 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine;
 using JazzyLucas.Core.Input;
 using JazzyLucas.Core.Utils;
-using UnityEngine;
 
 namespace JazzyLucas.Core
 {
     public class PerspectiveController : MonoBehaviour
     {
         [field: SerializeField] public Transform FirstPerson_ViewTransform { get; private set; }
-        [field: SerializeField] public Transform ThirdPerson_ViewTransform { get; private set; }
         [field: SerializeField] public Camera MainCamera { get; private set; }
+        [field: SerializeField] public float ThirdPersonDistance { get; private set; } = 5f;
+        [field: SerializeField] public LayerMask CollisionLayers { get; private set; } = ~0;
 
         public bool IsFirstPerson { get; private set; }
 
         private InputPoller inputPoller;
+
         private void Awake()
         {
-            inputPoller = new();
+            inputPoller = new InputPoller();
             if (!MainCamera)
                 MainCamera = Camera.main;
         }
@@ -33,10 +33,41 @@ namespace JazzyLucas.Core
             var input = inputPoller.PollInput();
             if (input.R)
             {
-                MainCamera.transform.SetParent(IsFirstPerson ? FirstPerson_ViewTransform : ThirdPerson_ViewTransform);
-                MainCamera.transform.localPosition = Vector3.zero;
                 IsFirstPerson = !IsFirstPerson;
+
+                if (IsFirstPerson)
+                {
+                    MainCamera.transform.SetParent(FirstPerson_ViewTransform);
+                    MainCamera.transform.localPosition = Vector3.zero;
+                }
+                else
+                {
+                    AdjustThirdPersonCamera();
+                }
             }
+
+            if (!IsFirstPerson)
+            {
+                AdjustThirdPersonCamera();
+            }
+        }
+
+        private void AdjustThirdPersonCamera()
+        {
+            var desiredPosition = FirstPerson_ViewTransform.position - FirstPerson_ViewTransform.forward * ThirdPersonDistance;
+            var directionToCamera = desiredPosition - FirstPerson_ViewTransform.position;
+
+            if (Physics.Raycast(FirstPerson_ViewTransform.position, directionToCamera, out RaycastHit hit, ThirdPersonDistance, CollisionLayers))
+            {
+                MainCamera.transform.position = hit.point - directionToCamera.normalized * 0.1f;
+            }
+            else
+            {
+                MainCamera.transform.position = desiredPosition;
+            }
+
+            MainCamera.transform.LookAt(FirstPerson_ViewTransform);
+            MainCamera.transform.SetParent(null);
         }
     }
 }
