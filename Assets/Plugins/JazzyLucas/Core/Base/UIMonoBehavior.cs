3f8c82b919b75a96cebc88a5b7284ed4dc8261ff
JazzyLucas <59397873+JazzyLucas@users.noreply.github.com>
2024-09-06 23:22:26 -0400

    Update UIMonoBehavior.cs

--- Assets/Plugins/JazzyLucas/Core/Base/UIMonoBehavior.cs
+++ Assets/Plugins/JazzyLucas/Core/Base/UIMonoBehavior.cs
@@ -6,23 +6,38 @@ using UnityEngine;
 namespace JazzyLucas.Core
 {
     /// <summary>
-    /// Use this class to make toggle-able UI without de-activating scripts.
+    /// Toggle-able UI without de-activating scripts.
     /// </summary>
+    /// <remarks>
+    /// Use within a Canvas.
+    /// </remarks>
     [RequireComponent(typeof(CanvasGroup))]
     public class UIMonoBehavior : MonoBehaviour
     {
+        [field:HideInInspector] public event Action<bool> OnVisibilityChanged;
+        [field:HideInInspector] public bool IsVisible => _canvasGroup.alpha > 0.1;
+        
         private CanvasGroup _canvasGroup;
-
-        public event Action<bool> OnVisibilityChanged;
         
         private void Awake()
+        {
+            OnAwake();
+        }
+
+        /// <summary>
+        /// Called in Unity's Awake.
+        /// </summary>
+        /// <remarks>
+        /// Please do base.OnAwake();
+        /// </remarks>
+        protected virtual void OnAwake()
         {
             _canvasGroup = GetComponent<CanvasGroup>();
         }
 
         public void SetVisibility(bool value)
         {
-            _canvasGroup.alpha = value ? 0 : 1;
+            _canvasGroup.alpha = value ? 1 : 0;
             _canvasGroup.blocksRaycasts = value;
             _canvasGroup.interactable = value;
             OnVisibilityChanged?.Invoke(value);
