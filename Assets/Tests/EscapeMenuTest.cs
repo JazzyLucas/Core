d7404e9c0a28beb93aaee03669a230d0522771cd
JazzyLucas <59397873+JazzyLucas@users.noreply.github.com>
2024-09-06 23:22:36 -0400

    added EscapeMenuTest

---
+++ Assets/Tests/EscapeMenuTest.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using JazzyLucas.Core;
+using JazzyLucas.Core.Input;
+using UnityEngine;
+
+public class EscapeMenuTest : UIMonoBehavior
+{
+    private InputPoller inputPoller;
+    
+    protected override void OnAwake()
+    {
+        inputPoller = new();
+        base.OnAwake();
+    }
+
+    private void Update()
+    {
+        var input = inputPoller.PollInput();
+
+        if (input.PauseEscape)
+        {
+            SetVisibility(!IsVisible);
+            Cursor.lockState = IsVisible ? CursorLockMode.None : CursorLockMode.Locked;
+        }
+
+    }
+}
