19504932e48fcd406adb514c700bb7e5f7955a25
JazzyLucas <59397873+JazzyLucas@users.noreply.github.com>
2024-09-09 23:53:41 -0400

    added Note util class

---
+++ Assets/Plugins/JazzyLucas/Core/Utils/Note.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JazzyLucas.Core
+{
+    [AddComponentMenu("JazzyLucas/Utilities/Note")]
+    public class Note : MonoBehaviour
+    {
+        [field: TextArea(3, 10)] 
+        [field: SerializeField] public string note { get; private set; } = "Add your note here!";
+    }
+}
